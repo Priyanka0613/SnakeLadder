9afe711b684aa2a1d229f6d5a60f48cb1344e4ed
Priyanka <priyankaag.0613@gmail.com>
2023-12-24 22:19:41 +0530

    uc2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Snake and Ladder!");
         int pos=0;
+        Random random = new Random();
+        int number = (random.nextInt(6)+1);
     }
 }
