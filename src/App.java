06ad75932ed59f00b42e83b44524e4c261be6d60
Priyanka <priyankaag.0613@gmail.com>
2023-12-24 22:29:10 +0530

    uc4

--- src/App.java
+++ src/App.java
@@ -3,13 +3,14 @@ import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Snake and Ladder!");
-        int pos=0;
+        int pos = 0;
         Random random = new Random();
         int number = (random.nextInt(6)+1);
-        int steps=0;
         Random rand = new Random();
         int choose = rand.nextInt(3); //0->no; 1->ladder; 2->snake
-        if(choose==1){steps+=number;}
-        else if(choose==2){steps-=number;}
+        while (pos<=100) {
+            if(choose==1){pos+=number;}
+            else if(choose==2 && pos-number>=0){pos-=number;}
+        }
     }
 }
