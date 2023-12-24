8d769391454d02dc104de02b27781fd5c4ab932c
Priyanka <priyankaag.0613@gmail.com>
2023-12-24 22:24:49 +0530

    uc3

--- src/App.java
+++ src/App.java
@@ -6,5 +6,10 @@ public class App {
         int pos=0;
         Random random = new Random();
         int number = (random.nextInt(6)+1);
+        int steps=0;
+        Random rand = new Random();
+        int choose = rand.nextInt(3); //0->no; 1->ladder; 2->snake
+        if(choose==1){steps+=number;}
+        else if(choose==2){steps-=number;}
     }
 }
