eb5b1f7f02c6cf1941ffa2ff7c8e278a72f535fd
Priyanka <priyankaag.0613@gmail.com>
2023-12-24 22:35:12 +0530

    uc6

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Snake and Ladder!");
         int pos = 0;
+        int steps=0;
         Random random = new Random();
         int number = (random.nextInt(6)+1);
         Random rand = new Random();
@@ -12,10 +13,15 @@ public class App {
             if(choose==1){
                 if(pos+number<100){
                     pos=pos+number;
+                    System.out.println("You are at position: "+pos);
                 }
-                else if(pos+number==100){System.out.println("WON!");}
+                else if(pos+number==100){System.out.println("WON! and the number of dices it took you: " + steps);}
             }
-            else if(choose==2 && pos-number>=0){pos-=number;}
+            else if(choose==2 && pos-number>=0){
+                pos-=number;
+                System.out.println("You are at position: "+pos);
+            }
+            steps+=1;
         }
     }
 }
